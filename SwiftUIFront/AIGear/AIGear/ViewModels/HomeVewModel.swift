da7cba81311592e33530e227a6667c31b7326a65
Dastan Sugirbay <sugirbay@Dastans-MacBook-Air-2.local>
2025-06-19 16:40:30 +0500

    MVVM architecture built

---
+++ SwiftUIFront/AIGear/AIGear/ViewModels/HomeVewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeVewModel.swift
+//  AIGear
+//
+//  Created by Dastan Sugirbay on 19.06.2025.
+//
+
+import Foundation
---
+++ SwiftUIFront/AIGear/AIGear/ViewModels/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  AIGear
+//
+//  Created by Dastan Sugirbay on 19.06.2025.
+//
+
+import Foundation
