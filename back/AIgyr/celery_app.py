acbf29858d281f775d9b391bbeab3caf5357005d
Dastan Sugirbay <dastan.sugirbay@gmail.com>
2025-07-01 17:33:48 +0500

    test

--- back/AIgyr/celery_app.py
+++ back/AIgyr/celery_app.py
@@ -1,17 +1,17 @@
-import os 
-import time
-from celery import Celery
-from dotenv import load_dotenv
+# import os 
+# import time
+# from celery import Celery
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-app = Celery(
-    "tasks",
-    broker=os.getenv("CELERY_BROKER_URL"),
-    backend=os.getenv("CELERY_RESULT_BACKEND"),
-)
+# app = Celery(
+#     "tasks",
+#     broker=os.getenv("CELERY_BROKER_URL"),
+#     backend=os.getenv("CELERY_RESULT_BACKEND"),
+# )
 
-@app.task(name = "create_task")
-def create_task(a, b, c):
-    time.sleep(a)
-    return b + c
+# @app.task(name = "create_task")
+# def create_task(a, b, c):
+#     time.sleep(a)
+#     return b + c
